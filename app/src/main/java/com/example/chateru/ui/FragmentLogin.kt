d8b2b8634fa675b423b2f52bcf7193da4dcfaee7
Ankur Singh <ankursingh71101@gmail.com>
2023-07-27 18:18:31 +0530

    Nav: Added Navigation via every Button or Image Button and fixed a few bugs
    Signed-off-by: Ankur Singh <ankursingh71101@gmail.com>

--- app/src/main/java/com/example/chateru/ui/FragmentLogin.kt
+++ app/src/main/java/com/example/chateru/ui/FragmentLogin.kt
@@ -4,16 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.chateru.R
+import com.example.chateru.databinding.FragmentLoginBinding
+import com.example.chateru.viewmodel.NavigationViewModel
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
 class FragmentLogin : Fragment() {
 
+    val navigationViewModel: NavigationViewModel by activityViewModels()
+
+    private var _binding : FragmentLoginBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClickListener()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun initClickListener(){
+        binding.tvBtnSignup.setOnClickListener{
+            navigationViewModel.showSignUpFragment()
+            Toast.makeText(requireContext(),"Clicked on Sign Up", Toast.LENGTH_LONG).show()
+        }
+        binding.btnLogin.setOnClickListener {
+            Toast.makeText(requireContext(),"Clicked on Login",Toast.LENGTH_LONG).show()
+        }
+        binding.btnLoginBack.setOnClickListener {
+            navigationViewModel.showGetStartedFragment()
+            Toast.makeText(requireContext(),"Clicked on Get Started",Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/chateru/ui/FragmentSignUp.kt
+++ app/src/main/java/com/example/chateru/ui/FragmentSignUp.kt
@@ -4,17 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.chateru.R
+import com.example.chateru.databinding.FragmentGetStartedBinding
+import com.example.chateru.databinding.FragmentSignUpBinding
+import com.example.chateru.viewmodel.NavigationViewModel
 
 class FragmentSignUp : Fragment() {
 
+    private val navigationViewModel: NavigationViewModel by activityViewModels()
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClickListener()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+    private fun initClickListener() {
+        binding.btnSignupBack.setOnClickListener {
+            navigationViewModel.showLoginFragment()
+            Toast.makeText(requireContext(),"Clicked on Back Button",Toast.LENGTH_LONG).show()
+        }
+        binding.btnSignup.setOnClickListener {
+            Toast.makeText(requireContext(),"Clicked on Sign Up Button",Toast.LENGTH_LONG).show()
+        }
+
     }
 
 }
